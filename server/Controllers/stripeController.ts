94d445e4bdc8ac307c7ac902e1d757ebbcc996d9
James Hocking <jameshocking542@gmail.com>
2023-11-22 14:44:02 +1100

    adding so that there is  camps on discount

--- server/Controllers/stripeController.ts
+++ server/Controllers/stripeController.ts
@@ -6,6 +6,7 @@ import generateHashedTokens, {hashedTokensType} from "../util/randomToken";
 import Tokens from "../Models/Tokens";
 import Product from "../Models/Product";
 import { ses, senderEmail } from "../util/emails";
+import { useTokenForPlan } from "../util/randomToken"
 
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
 
@@ -51,7 +52,7 @@ const storeItems = new Map([
     [14, { priceInCents: 50, name: "1 on 1 Private (Plan)"}],
     [15, { priceInCents: 50, name: "Group Private (Plan)"}],
     [16, { priceInCents: 10000, name: "Holiday Camp (1 day)"}], 
-    [17, { priceInCents: 5500, name:"Discounted Camp"}]
+    [17, { priceInCents: 10000, name:"$100 Discounted Camp"}],
 ])
 
 async function doesCustomerExist(customerEmail:string, customerName:string){
@@ -122,32 +123,7 @@ async function makeTokenForPlan(id:number, customerEmail:string){
     }
 }
 
-async function useTokenForPlan(token:string, id:number){
-    const typeOfToken = (id == 14) ? "singleTokens":"groupTokens";
-    
-    let filter = {};
 
-    const allHashedTokens:Array<hashedTokensType> = await Tokens.find();
-    const ActualTokens:hashedTokensType = allHashedTokens[0];
-    let searching = [""];
-
-    if (id == 14){
-        searching = ActualTokens.singleTokens;
-    } else {
-        searching = ActualTokens.groupTokens;
-    }
-    
-    let indexer = 0;
-    for (let i = 0; i < searching.length; i++){
-        let isMatch = await bcrypt.compareSync(token?token:"", searching[i]);
-        if (isMatch){
-            indexer = i;
-        }     
-    }
-
-    const update = { $pull: { [typeOfToken]: searching[indexer] } };
-    const updatedToken = await Tokens.findOneAndUpdate(filter, update, { new:true, runValidators:true});
-}
 
 export const stripeController = {
     createSession: async (req: Request, res: Response) => { //function for handling when a payment session is beginning
--- server/util/randomToken.ts
+++ server/util/randomToken.ts
@@ -5,9 +5,14 @@ import express, { NextFunction, Request, Response } from "express";
 const SALT_ROUNDS = 10;
 
 export type hashedTokensType = {
-    singleTokens: Array<string>;
-    groupTokens: Array<string>;
-    campTokens: Array<string>;
+    singleTokens: Array<Token>;
+    groupTokens: Array<Token>;
+    campTokens: Array<Token>;
+}
+
+type Token = {
+    coupon: string,
+    item_name: string
 }
 
 /*
@@ -38,10 +43,10 @@ export default async function generateHashedTokens(amount:number, length:number)
     return [hashedTokens, nonhashedTokens];
 }
 
-async function isTokenInValidTokens(tokensToSearch:string[], tokenToMatch:string, isHashing:boolean){
+async function isTokenInValidTokens(tokensToSearch:Token[], tokenToMatch:string, isHashing:boolean){
     for (let i = 0; i < tokensToSearch.length; i++){
         if (!isHashing){
-            if (tokensToSearch[i] == tokenToMatch){
+            if (tokensToSearch[i]["coupon"] == tokenToMatch){
                 return true
             }   
         } else {
@@ -55,6 +60,33 @@ async function isTokenInValidTokens(tokensToSearch:string[], tokenToMatch:string
     return false
 }
 
+export async function useTokenForPlan(token:string, id:number){
+    const typeOfToken = (id == 14) ? "singleTokens":"groupTokens";
+    
+    let filter = {};
+
+    const allHashedTokens:Array<hashedTokensType> = await Tokens.find();
+    const ActualTokens:hashedTokensType = allHashedTokens[0];
+    let searching: Array<Token> = [];
+
+    if (id == 14){
+        searching = ActualTokens.singleTokens;
+    } else {
+        searching = ActualTokens.groupTokens;
+    }
+    
+    let indexer = 0;
+    for (let i = 0; i < searching.length; i++){
+        let isMatch = await bcrypt.compareSync(token?token:"", searching[i]);
+        if (isMatch){
+            indexer = i;
+        }     
+    }
+
+    const update = { $pull: { [typeOfToken]: searching[indexer] } };
+    const updatedToken = await Tokens.findOneAndUpdate(filter, update, { new:true, runValidators:true});
+}
+
 export const tokenController = {
     checkToken: async (req: Request, res: Response) => { // function that checks whether a token is valid for the right id
         try {
@@ -64,7 +96,7 @@ export const tokenController = {
             // Find all documents in the collection and get an array of hashed tokens
             const allHashedTokens:Array<hashedTokensType> = await Tokens.find();
             const ActualTokens:hashedTokensType = allHashedTokens[0];
-            let searching = [""];
+            let searching: Array<Token> = [];
             let hashing = false;
     
             if (id == 3){ //single private session
