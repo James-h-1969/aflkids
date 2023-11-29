5748c1d81deb8fd8cb6181aed2fd266bbaaecaa2
James Hocking <jameshocking542@gmail.com>
2023-11-29 14:05:29 +1100

    weird git stuff

--- client/src/pages/ManagerHelp/AddCamp.tsx
+++ client/src/pages/ManagerHelp/AddCamp.tsx
@@ -5,7 +5,6 @@ import { ColourScheme, backendLink } from "../../globalVar";
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
-import { Cpu } from "react-bootstrap-icons";
 
 
 type AddCampProps = {
