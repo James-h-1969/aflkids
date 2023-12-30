13c1b7b1e0c83899c7ffed2a8572c102bf37ca5f
James Hocking <jameshocking542@gmail.com>
2023-12-30 11:12:48 +1100

    links at bottom of the page now work

--- client/src/pages/Components/Footer.tsx
+++ client/src/pages/Components/Footer.tsx
@@ -12,6 +12,7 @@ export default function Footer(){
     function changeURL(url:string){
         window.parent.postMessage(url, "https://www.aflkids.com.au/") // so wix changes its url\
     }
+
     return(<>
         <div className="d-flex justify-content-around text-center" style={{gap:mediaQueries.mobile?"0px":"20px", height:mediaQueries.mobile?"100px":"300px", fontFamily:"Rubik", fontWeight:"bold", fontSize:mediaQueries.mobile?"12px":"40px"}}>
             <div>
@@ -28,15 +29,13 @@ export default function Footer(){
             <div>
                 Thanks to our partnered clubs
                 <div className="d-flex justify-content-around align-items-center pt-4" style={{gap:mediaQueries.mobile?"0px":"10px"}}>
-                    <Image src="/assets/Logos/bombers.jpeg" onClick={() => changeURL("https://www.manlybombers.com.au/")} style={{width:mediaQueries.mobile?"30px":"90px"}}/>
-                    <Image src="/assets/Logos/Lions.jpeg" onClick={() => changeURL("https://forestafl.com.au/")} style={{width:mediaQueries.mobile?"30px":"90px"}}/>
-                    <Image src="/assets/Logos/stives.jpeg" onClick={() => changeURL("https://stivesafl.teamapp.com/?_webpage=v1")} style={{width:mediaQueries.mobile?"30px":"90px"}}/>
-                    <Image src="/assets/Logos/swans.png" onClick={() => changeURL("https://mosmanswans.com.au/")} style={{width:mediaQueries.mobile?"30px":"90px"}}/>
-                    <Image src="/assets/Logos/Tigers.png" onClick={() => changeURL("https://www.pittwatertigers.com.au/")} style={{width:mediaQueries.mobile?"30px":"90px"}}/>
-                    <Image src="/assets/Logos/eagles.jpg" onClick={() => changeURL("https://hornsbyberowraeagles.com/")} style={{width:mediaQueries.mobile?"30px":"90px"}}/>
-                    <Link to="https://wildcatsafl.com/">
-                        <Image src="/assets/Logos/wildcats.jpg" onClick={() => changeURL("https://www.manlybombers.com.au/")} style={{width:mediaQueries.mobile?"30px":"90px"}}/>
-                    </Link>
+                    <Image src="/assets/Logos/bombers.jpeg" onClick={() => changeURL("https://www.manlybombers.com.au/")} style={{width:mediaQueries.mobile?"30px":"90px", cursor:"pointer"}}/>
+                    <Image src="/assets/Logos/Lions.jpeg" onClick={() => changeURL("https://forestafl.com.au/")} style={{width:mediaQueries.mobile?"30px":"90px", cursor:"pointer"}}/>
+                    <Image src="/assets/Logos/stives.jpeg" onClick={() => changeURL("https://stivesafl.teamapp.com/?_webpage=v1")} style={{width:mediaQueries.mobile?"30px":"90px", cursor:"pointer"}}/>
+                    <Image src="/assets/Logos/swans.png" onClick={() => changeURL("https://mosmanswans.com.au/")} style={{width:mediaQueries.mobile?"30px":"90px", cursor:"pointer"}}/>
+                    <Image src="/assets/Logos/Tigers.png" onClick={() => changeURL("https://www.pittwatertigers.com.au/")} style={{width:mediaQueries.mobile?"30px":"90px", cursor:"pointer"}}/>
+                    <Image src="/assets/Logos/eagles.jpg" onClick={() => changeURL("https://hornsbyberowraeagles.com/")} style={{width:mediaQueries.mobile?"30px":"90px", cursor:"pointer"}}/>
+                    <Image src="/assets/Logos/wildcats.jpg" onClick={() => changeURL("https://www.manlybombers.com.au/")} style={{width:mediaQueries.mobile?"30px":"90px", cursor:"pointer"}}/>
                 </div>
             </div>
         </div>
